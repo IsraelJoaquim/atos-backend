2372ad84eb271d6b57d358fbe89bf8049b8af726
israelcoder <israelgcjoaquim@gmail.com>
2026-05-31 17:32:36 -0300

    fiz route webhook novamente

--- src/app.js
+++ src/app.js
@@ -1,8 +1,6 @@
 import fastifyCors from '@fastify/cors';
 import { fastify } from 'fastify';
 import fastifyMultipart from '@fastify/multipart';
-
-
 import authRoutes from './routes/authRoutes.js';
 import ticketRoutes from './routes/ticketRoutes.js';
 import tenantRoutes from './routes/tenantRoutes.js';
@@ -11,24 +9,24 @@ import webhookRoute from './routes/webhookRoute.js';
 const server = fastify();
 
 server.addHook('onRequest', (request, response, done) => {
-
-  // ignora o webhook
   if (request.url === '/webhook/email') {
     return done();
   }
-
   const contentType = request.headers['content-type'];
   if (contentType !== 'application/json') {
     response.header('content-type', 'application/json');
   }
-
   done();
 });
 
+server.register(fastifyCors, {
+  origin: '*',
+  allowedHeaders: ['Content-Type', 'Authorization'],
+});
+server.register(fastifyMultipart);
 server.register(authRoutes);
 server.register(ticketRoutes);
 server.register(tenantRoutes);
-server.register(fastifyMultipart)
 server.register(webhookRoute);
 
 server.get('/', async (_, response) => {
